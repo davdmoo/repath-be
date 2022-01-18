25f129bae44af901aa05892c1eaa56258352b415
davdmoo <david.mulyawan97@gmail.com>
2022-01-18 22:15:06 +0700

    Feat: authentication

---
+++ middlewares/authentication.js
@@ -0,0 +1,28 @@
+const userModel = require('../models/userModel');
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.SECRETKEY;
+const { ObjectId } = require('mongodb');
+
+async function authentication (req, res, next) {
+  try {
+    const access_token = req.headers.access_token;
+    if (!access_token) throw { name: "TokenNotFound" };
+
+    const payload = jwt.verify(access_token, secretKey);
+    if (!payload) throw { name: "InvalidToken" };
+
+    const user = await userModel.findOne({ email: payload.email });
+    if (!user) throw { name: "JsonWebTokenError" };
+
+    req.user = {
+      id: ObjectId(user._id),
+      email: user.email
+    };
+    next();
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+}
+
+module.exports = authentication;
