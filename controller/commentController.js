0ad5093c27a5b0211c690c6464497dc6d8bfb348
siska <siska25.oktaviani@gmail.com>
2022-01-18 19:15:29 +0700

    adding controller skeleton in comments/likes

---
+++ controller/commentController.js
@@ -0,0 +1,65 @@
+const commentModel = require('../models/commentModel')
+
+class Comment{
+    static async findComments(req, res){
+        try{
+            const id = req.params.id
+            console.log(id, req.params);
+            // const comments = await commentModel.find().exec()
+
+            // res.status(200).json(comments)
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+    static async addComment(req, res){
+        try{
+            req.body.user = req.headers.keyid
+            const newComment = await commentModel.create(req.body)
+
+            res.status(201).json(newComment)
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+    static async findComment(req, res){
+        try{
+            const id = req.params.id
+            const comment = await commentModel.findOne({_id: id}).exec()
+
+            res.status(200).json(comment)
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+    static async editComment(req, res){
+        try{
+            const id = req.params.id
+            
+            await commentModel.updateOne({_id: id}, req.body)
+            const updatedComment = await commentModel.find({_id: id}).exec()
+
+            res.status(200).json(updatedComment)
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+    static async deleteComment(req, res){
+        try{
+            const id = req.params.id
+            const deletedComment = await commentModel.find({_id: id}).exec()
+            await commentModel.deleteOne({_id: id})
+
+            res.status(201).json(deletedComment)
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }
+
+}
+
+module.exports = Comment
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,3 @@ router.patch('/:id', userController.editUser)
 router.delete('/:id', userController.deleteUser)
 
 module.exports = router
-
-// 1 login register
-// 2 setup sisa model
-// 3 setup routes/controller (update/delete user & post)
