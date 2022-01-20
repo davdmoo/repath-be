ff558cab4e04b03302596ec11390e02121875dc3
siska <siska25.oktaviani@gmail.com>
2022-01-20 19:38:40 +0700

    try merge

--- routes/friendRoutes.js
+++ routes/friendRoutes.js
@@ -3,7 +3,6 @@ const friendController = require('../controller/friendController')
 
 router.get('/', friendController.findFriends)
 router.post('/:friendId', friendController.addFriend)
-router.get('/:friendId', friendController.findFriends)
 router.delete('/:friendId', friendController.deleteFriend)
 
 module.exports = router
