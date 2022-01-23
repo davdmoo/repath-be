ce7d57d01a33e20c931e0e35e1255fbfa41cea5d
siska <siska25.oktaviani@gmail.com>
2022-01-23 21:41:32 +0700

    fix bug in schema

--- __test__/comment.test.js
+++ __test__/comment.test.js
@@ -50,7 +50,6 @@ describe("GET /comments", () => {
         .set('access_token', null)
         .then((resp) => {
             const result = resp.body
-            console.log(result);
             expect(resp.statusCode).toBe(401)
             expect(resp.res.statusMessage).toMatch("Unauthorized")
             expect(result).toMatchObject({"message": 'Invalid token'})
