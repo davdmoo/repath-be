43f4a4393bc891efd908ea47e1381f4f274c6ed6
siska <siska25.oktaviani@gmail.com>
2022-01-23 12:07:09 +0700

    fetching friend already accepted

--- controller/friendController.js
+++ controller/friendController.js
@@ -1,17 +1,21 @@
 const friendModel = require("../models/friendModel")
 const userModel = require('../models/userModel')
+
 class Friend{
     static async findFriends(req, res, next){
         try {
             const {id} = req.user
             const friends = await userModel.findById(id).populate("friends")
-            
+            friends.friends.filter(el => {
+                if(el.sender == id || el.receiver == id){
+                    return el
+                }
+            })
             res.status(200).json(friends.friends)
         } catch (error) {
             next(error)
         }
     }
-
 
     static async addFriend(req, res, next){
         try {
@@ -44,12 +48,27 @@ class Friend{
     static async accFriend(req, res, next){
         try {
             const {id} = req.user
-            const {followId} = req.params
+            const {reqId} = req.params
+
+            const friendReq = await friendModel.findById(reqId)
             const request = await friendModel.findOneAndUpdate(
-                {sender: followId ,receiver: id},
+                {_id: reqId},
                 {
-                    sender: followId ,receiver: id, status: true
+                    status: true
                 })
+            
+            await userModel.findOneAndUpdate({_id: friendReq.receiver},
+                {
+                    $push: {
+                    friends: request
+                    }
+                });
+            await userModel.findOneAndUpdate({_id: friendReq.sender},
+                {
+                    $push: {
+                    friends: request
+                    }
+                });
             res.status(200).json(request)
         } catch (error) {
             next(error)
@@ -59,7 +78,7 @@ class Friend{
     static async delFriend(req, res, next){
         try {
             const {id} = req.user
-            const {followId} = req.params
+            const {reqId} = req.params
             const decline = await friendModel.findOne({sender: followId, receiver: id})
             friendModel.deleteOne({sender: followId, receiver: id})
 
