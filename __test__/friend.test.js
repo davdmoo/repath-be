ca691dd0e554457f6d83044d1b4d54bd015ecc48
davdmoo <david.mulyawan97@gmail.com>
2022-01-24 11:01:57 +0700

    Fix friend fetching

--- __test__/friend.test.js
+++ __test__/friend.test.js
@@ -230,7 +230,7 @@ describe("DELETE /friends", () => {
         })
         .then((resp)=>{
             const result = resp.body
-            expect(resp.status).toBe(201)
+            expect(resp.status).toBe(200)
             expect(result).toEqual(expect.any(Object))
             done()
         })
@@ -248,9 +248,9 @@ describe("DELETE /friends", () => {
         })
         .then((resp)=>{
             const result = resp.body
-            expect(resp.status).toBe(400)
-            expect(resp.res.statusMessage).toMatch("Bad Request")
-            expect(result).toEqual({message: 'Content not found'})
+            expect(resp.status).toBe(403)
+            expect(resp.res.statusMessage).toMatch("Forbidden")
+            expect(result).toEqual({message: 'Forbidden access'})
             done()
         })
         .catch((err)=>{
